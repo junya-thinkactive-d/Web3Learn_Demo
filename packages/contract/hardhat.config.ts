ddc0107825cf86bff6201a76377f2ab419a7b896
junya_tad <54170086+junya-thinkactive-d@users.noreply.github.com>
2022-12-17 16:07:20 +0900

    Initial commit

---
+++ packages/contract/hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@typechain/hardhat'
+import 'hardhat-contract-sizer'
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+
+const config: HardhatUserConfig = {
+  solidity:{
+    version: '0.8.17',
+    // settings:{
+    //   optimizer: {
+    //     enabled: true,
+    //     runs: 200,
+    //   },
+  },
+  typechain: {
+    outDir: "../client/src/libs/hardhat/types" && "./types", 
+    target: "ethers-v5",
+    alwaysGenerateOverloads: false,
+    externalArtifacts: ["externalArtifacts/*.json"],
+    dontOverrideCompile: false,
+  },
+  paths: {
+    artifacts: '../client/src/libs/hardhat/artifacts',
+  },
+};
+
+export default config;
